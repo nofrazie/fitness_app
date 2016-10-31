c5197fdbfaed960627404ea08b23c9be93f3d365
Nolan Frazier <nofrazier3@gmail.com>
2016-10-30 22:05:53 -0700

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root  'static_pages#home'
-  get   '/home'     =>  'static_pages#home'
   get   '/help'     =>  'static_pages#help'
   get   '/about'    =>  'static_pages#about'
   get   '/contact'  =>  'static_pages#contact'
@@ -14,4 +11,5 @@ Rails.application.routes.draw do
   post    '/login'    =>  'sessions#create'
   delete  '/logout'   =>  'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
