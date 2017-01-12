fe7b6233ac3c5e23e1233b89f453555a48724d32
Nolan Frazier <nofrazier3@gmail.com>
2017-01-11 19:02:44 -0800

    Added edit functionality for exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,9 +1,8 @@
 class ExercisesController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :set_workout_and_exercise, only: [:show, :edit, :update, :destroy]
 
   def show
-    @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.find_by_id(params[:id])
     @sets = @exercise.exercise_sets
   end
 
@@ -23,9 +22,19 @@ class ExercisesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @exercise.update_attributes(exercise_params)
+      flash[:success] = "This exercise has been updated!"
+      redirect_to workout_exercise_path(@workout, @exercise)
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
-    @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.find(params[:id])
     @exercise.destroy
     flash[:success] = "Exercise deleted"
     redirect_to workout_path(@workout)
@@ -34,5 +43,10 @@ class ExercisesController < ApplicationController
   private
     def exercise_params
       params.require(:exercise).permit(:name, body_part_ids:[])
+    end
+
+    def set_workout_and_exercise
+      @workout = Workout.find(params[:workout_id])
+      @exercise = @workout.exercises.find_by_id(params[:id])
     end
 end
--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,3 +1,7 @@
 class ExerciseSet < ApplicationRecord
   belongs_to :exercise
+  validates :number,  presence: true,
+                      uniqueness: true
+  validates :reps,    presence: true
+  validates :weight,  presence: true
 end
