dd3a4cfdb5baf46cd95eae3079f2ab9d1aa5b22d
Nolan Frazier <nofrazier3@gmail.com>
2017-01-11 19:11:24 -0800

    Added update and delete functionality for exercise_sets

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -1,5 +1,6 @@
 class ExerciseSetsController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :get_workout_exercise_set, only: [:edit, :update, :destroy]
 
   def new
     @workout = Workout.find(params[:workout_id])
@@ -19,10 +20,19 @@ class ExerciseSetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @set.update_attributes(set_params)
+      flash[:success] = "You've updated set number " + @set.number.to_s + "!" 
+      redirect_to workout_exercise_path(@workout, @exercise)
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
-    @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.find(params[:exercise_id])
-    @set = @exercise.exercise_sets.find(params[:id])
     @set.destroy
     flash[:success] = "Set deleted!"
     redirect_to workout_exercise_path(@workout, @exercise)
@@ -31,5 +41,11 @@ class ExerciseSetsController < ApplicationController
   private
     def set_params
       params.require(:exercise_set).permit(:number, :reps, :weight, :exercise_id)
+    end
+
+    def get_workout_exercise_set
+      @workout = Workout.find(params[:workout_id])
+      @exercise = @workout.exercises.find(params[:exercise_id])
+      @set = @exercise.exercise_sets.find(params[:id])
     end
 end
