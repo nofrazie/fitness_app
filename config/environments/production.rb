4b9a9236a5d05543321af84cb03f92981ec904b6
Nolan Frazier <nofrazier3@gmail.com>
2017-01-13 12:47:38 -0800

    Fixed sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ancient-shore-69994.herokuapp.com'
+  host = 'protected-meadow-33769.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
